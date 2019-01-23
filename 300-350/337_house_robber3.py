2c89eb0f7ea9c51c16c66c209236859734ba6ed3
Dolantinlist <bupt_dolantin@163.com>
2019-01-23 20:36:21 +0800

    house robber

--- 300-350/337_house_robber3.py
+++ 300-350/337_house_robber3.py
@@ -1,1 +1,13 @@
 from Tree import *
+class Solution:
+    def rob(self, root):
+        return max(self.helper(root))
+    def helper(self, node):
+        if not node:
+            return [0, 0]  # 0:抢自己, 1:不抢自己, 2:环路
+        l = self.helper(node.left)
+        r = self.helper(node.right)
+        return [l[1] + r[1] + node.val, max(r[0], r[1]) + max(l[0], l[1])]
+
+root = creatTree([3,4,5,1,3,None,1])
+print(Solution().rob(root))
