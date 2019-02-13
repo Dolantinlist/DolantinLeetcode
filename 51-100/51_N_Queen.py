4e46774d4804ca2dd9dc47489c5773382550da1a
liuying <liuying@momenta.ai>
2019-02-13 10:29:16 +0800

    change

--- 51-100/51_N_Queen.py
+++ 51-100/51_N_Queen.py
@@ -1,25 +1,45 @@
 class Solution(object):
     def solveNQueens(self, n):
-        self.n = n
-        self.res = []
+        res = []
         rlt = []
-        self.dfs([],[],[])
-        for i in self.res:
+        if not n:
+            return None
+        self.dfs(n, [], [], [], res)
+        for ans in res:
             tmp = []
-            for j in i:
+            for j in ans:
                 tmp += ["." * j + "Q" + "." * (n - 1 - j)]
             rlt.append(tmp)
         return rlt
-
-    def dfs(self, queen, add, diff):
+    def dfs(self, n, queen, add, diff, res):
         i = len(queen)
-        if len(queen) == self.n:
-            self.res.append(queen)
-            return
-        else:
-            for j in range(self.n):
-                if j not in queen and i + j not in add and i - j not in diff:
-                    self.dfs(queen + [j], add + [i + j], diff + [i - j])
+        if i == n:
+            res.append(queen)
+        for j in range(n):
+            if j not in queen and i + j not in add and i - j not in diff:
+                self.dfs(n, queen + [j], add + [i + j], diff + [i - j], res)
+
+    # def solveNQueens(self, n):
+    #     self.n = n
+    #     self.res = []
+    #     rlt = []
+    #     self.dfs([],[],[])
+    #     for i in self.res:
+    #         tmp = []
+    #         for j in i:
+    #             tmp += ["." * j + "Q" + "." * (n - 1 - j)]
+    #         rlt.append(tmp)
+    #     return rlt
+    #
+    # def dfs(self, queen, add, diff):
+    #     i = len(queen)
+    #     if len(queen) == self.n:
+    #         self.res.append(queen)
+    #         return
+    #     else:
+    #         for j in range(self.n):
+    #             if j not in queen and i + j not in add and i - j not in diff:
+    #                 self.dfs(queen + [j], add + [i + j], diff + [i - j])
 
 
 print(Solution().solveNQueens(4))
